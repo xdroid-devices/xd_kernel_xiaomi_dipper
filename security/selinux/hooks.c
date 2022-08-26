b6cc8faf07c84b060b5ae6ab84b0149320a2a9b1
Chenyang Zhong <zhongcy95@gmail.com>
2022-08-26 10:33:34 -0400

    Revert "selinux: fix error initialization in inode_doinit_with_dentry()"
    We have it already at 7ffddeed824e6cec9c08a622329e757f0301c332.
    
    This reverts commit c00fcec32cd2559529438bda6b3ae98c2e43ceeb.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -1480,13 +1480,7 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			 * inode_doinit with a dentry, before these inodes could
 			 * be used again by userspace.
 			 */
-			isec->initialized = LABEL_INVALID;
-			/*
-			 * There is nothing useful to jump to the "out"
-			 * label, except a needless spin lock/unlock
-			 * cycle.
-			 */
-			return 0;
+			goto out;
 		}
 
 		len = INITCONTEXTLEN;
@@ -1596,15 +1590,8 @@ static int inode_doinit_with_dentry(struct inode *inode, struct dentry *opt_dent
 			 * inode_doinit() with a dentry, before these inodes
 			 * could be used again by userspace.
 			 */
-			if (!dentry) {
-				isec->initialized = LABEL_INVALID;
-				/*
-				 * There is nothing useful to jump to the "out"
-				 * label, except a needless spin lock/unlock
-				 * cycle.
-				 */
-				return 0;
-			}
+			if (!dentry)
+				goto out;
 			rc = selinux_genfs_get_sid(dentry, sclass,
 						   sbsec->flags, &sid);
 			dput(dentry);
